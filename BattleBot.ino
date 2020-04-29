9cf45d081ca22591d7401d20aa40929de3b19f8b
Daniel Love <danlove99@hotmail.co.uk>
2020-04-29 13:32:25 +0100

    Filled in the Backward function

--- BattleBot.ino
+++ BattleBot.ino
@@ -31,29 +31,46 @@ void leftAttack()
 
 void forward()
 {
- pwm.setPWM(lf,0,pulseWidth(80)
- pwm.setPWM(rf,0,pulseWidth(80)
- delay(200)
- pwm.setPWM(ll,0,pulseWidth(110)
- pwm.setPWM(rl,0,pulseWidth(110)
- delay(200)
- pwm.setPWM(lf,0,pulseWidth(90)
- pwm.setPWM(rf,0,pulseWidth(90)
- delay(200)
- pwm.setPWM(lf,0,pulseWidth(100)
- pwm.setPWM(rf,0,pulseWidth(100)
- delay(200)
- pwm.setPWM(ll,0,pulseWidth(70)
- pwm.setPWM(rl,0,pulseWidth(70)
- delay(200)
- pwm.setPWM(lf,0,pulseWidth(90)
- pwm.setPWM(rf,0,pulseWidth(90)
- delay(200)
+ pwm.setPWM(lf,0,pulseWidth(80);
+ pwm.setPWM(rf,0,pulseWidth(80);
+ delay(200);
+ pwm.setPWM(ll,0,pulseWidth(110);
+ pwm.setPWM(rl,0,pulseWidth(110);
+ delay(200);
+ pwm.setPWM(lf,0,pulseWidth(90);
+ pwm.setPWM(rf,0,pulseWidth(90);
+ delay(200);
+ pwm.setPWM(lf,0,pulseWidth(100);
+ pwm.setPWM(rf,0,pulseWidth(100);
+ delay(200);
+ pwm.setPWM(ll,0,pulseWidth(70);
+ pwm.setPWM(rl,0,pulseWidth(70);
+ delay(200):
+ pwm.setPWM(lf,0,pulseWidth(90);
+ pwm.setPWM(rf,0,pulseWidth(90);
+ delay(200);
 }
 
 void backward()
 {
- pass;
+ pwm.setPWM(lf,0,pulseWidth(80);
+ pwm.setPWM(rf,0,pulseWidth(80);
+ delay(200);
+ pwm.setPWM(ll,0,pulseWidth(70);
+ pwm.setPWM(rl,0,pulseWidth(70);
+ delay(200);
+ pwm.setPWM(lf,0,pulseWidth(90);
+ pwm.setPWM(rf,0,pulseWidth(90);
+ delay(200);
+ pwm.setPWM(lf,0,pulseWidth(100);
+ pwm.setPWM(rf,0,pulseWidth(100);
+ delay(200);
+ pwm.setPWM(ll,0,pulseWidth(110);
+ pwm.setPWM(rl,0,pulseWidth(110);
+ delay(200);
+ pwm.setPWM(lf,0,pulseWidth(90);
+ pwm.setPWM(rf,0,pulseWidth(90);
+ delay(200);
 }
 
 int pulseWidth(int angle)
